76e61d38ff9d60b474a5cd0ecd91f7182626751d
Elliot <elliot.suhr@gmail.com>
2023-06-07 23:21:29 -0400

    netlify form fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -295,7 +295,7 @@
 		<img src="/sig.png" alt="" bind:this={teamphoto} style="visibility: hidden;">
 	</div>
 	<div class="emailContainer" bind:this={emailContainer}>
-		<form bind:this={form} method="POST" on:submit={submitFunc} data-netlify="true">
+		<form bind:this={form} method="POST" on:submit={submitFunc} netlify>
 			<input type="email" class="email" bind:value={inputText} bind:this={emailInput} on:input={emailFunc}>
 			<button type="submit" class="emailbutton" bind:this={emailButton} contenteditable="true" >{buttonText}</button>
 		</form>
